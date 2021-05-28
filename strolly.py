b0a85c1e1f9fc9cf67fe36b90cbab34ac5468269
dnns01 <git@dnns01.de>
2021-05-28 18:25:42 +0200

    Add notification to yuni, whenever Marcus starts blender

--- strolly.py
+++ strolly.py
@@ -15,8 +15,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 ACTIVITY = os.getenv('DISCORD_ACTIVITY')
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', help_command=None, activity=discord.Game(ACTIVITY), intents=intents)
 bot.add_cog(PollCog(bot))
 bot.add_cog(RollCog(bot))
@@ -28,6 +27,16 @@ bot.add_cog(Schedule(bot))
 @bot.event
 async def on_ready():
     print("Client started!")
+
+
+@bot.event
+async def on_member_update(before, after):
+    if before.id == 250613346653569025:
+        if after.activity and "Blender" in after.activity.name:
+            if not before.activity or (before.activity and "Blender" not in before.activity.name):
+                channel = await bot.fetch_channel(811980578621620235)
+                await channel.send(
+                    "Achtung <@!490167202625093634>!!! Er tut es schon wieder! Marcus hat gerade die Kultsoftware Blender gestartet!")
 
 
 bot.run(TOKEN)
